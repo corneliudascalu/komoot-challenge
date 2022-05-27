a2af43c9a238efa99f3e682f89268c50d895abe1
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2022-05-27 20:59:47 +0300

    Request location permission

--- app/src/main/java/com/corneliudascalu/challenge/MainActivity.kt
+++ app/src/main/java/com/corneliudascalu/challenge/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.corneliudascalu.challenge
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.corneliudascalu.challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var started = false
     val adapter = PhotoAdapter()
+    lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
     // TODO
     val viewModel = WalkViewModel()
@@ -30,17 +37,23 @@ class MainActivity : AppCompatActivity() {
             adapter.submitList(state.photos.map { it.url })
             binding.recyclerView.scrollToPosition(0)
         }
+
+        preparePermissionLauncher()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.clear()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.clear()
         menuInflater.inflate(if (started) R.menu.menu_stop else R.menu.menu_start, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menuStart) {
-            viewModel.start()
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                viewModel.start()
+            } else {
+                requestPermission()
+            }
             return true
         }
         if (item.itemId == R.id.menuStop) {
@@ -48,5 +61,27 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun preparePermissionLauncher() {
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                viewModel.start()
+            } else {
+
+                // TODO Only call this once more, and afterwards display a custom explanation
+                Toast.makeText(this, "Please grant access to fine location", Toast.LENGTH_SHORT).show()
+                permissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION))
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        val fineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (fineLocationPermission == PackageManager.PERMISSION_DENIED) {
+            permissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION))
+        } else {
+            viewModel.start()
+        }
     }
 }
